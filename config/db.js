3bfb1ad88701187dbc6f6df3dc62ec3b656f41f9
Aashu020 <aashunadaf070@gmail.com>
2025-03-15 13:48:53 +0530

    url changes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const URL = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.2";
-const URL ="mongodb+srv://root:root@cluster0.ca2cg9i.mongodb.net/ESchool?retryWrites=true&w=majority&appName=Cluster0";
+const URL ="mongodb+srv://root:root@cluster0.ca2cg9i.mongodb.net/geniustestingv3?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
   .connect(URL)
